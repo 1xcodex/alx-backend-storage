53efe201f6f8c070a737ac765bf54afa8be2bd96
1xcodex <yishak.ab1216@gmail.com>
2023-01-18 21:51:47 +0300

    edited

--- 0x02-redis_basic/web.py
+++ 0x02-redis_basic/web.py
@@ -1,54 +1,32 @@
 #!/usr/bin/env python3
-"""
-Module web
-"""
+""" Redis Module """
+
+from functools import wraps
 import redis
 import requests
 from typing import Callable
 
+redis_ = redis.Redis()
 
-_redis = redis.Redis()
 
+def count_requests(method: Callable) -> Callable:
+    """ Decortator for counting """
+    @wraps(method)
+    def wrapper(url):  # sourcery skip: use-named-expression
+        """ Wrapper for decorator """
+        redis_.incr(f"count:{url}")
+        cached_html = redis_.get(f"cached:{url}")
+        if cached_html:
+            return cached_html.decode('utf-8')
+        html = method(url)
+        redis_.setex(f"cached:{url}", 10, html)
+        return html
 
-def count_access(func: Callable) -> Callable:
-    """
-    Counts the number of times
-    a url was accessed
-    """
-    def wrapper(*args, **kwargs):
-        """wrapper"""
-        key = "count:{}".format(args[0])
-        _redis.incr(key)
-        return func(*args, **kwargs)
     return wrapper
 
 
-def get_cache(func: Callable) -> Callable:
-    """
-    Counts the number of times
-    a url was accessed
-    """
-    def wrapper(*args, **kwargs):
-        """wrapper"""
-        key = "result:{}".format(args[0])
-        if _redis.exists(key):
-            data = _redis.get(key)
-            return data.decode('utf-8')
-        return func(*args, **kwargs)
-    return wrapper
-
-
-@get_cache
-@count_access
+@count_requests
 def get_page(url: str) -> str:
-    """
-    Obtains the HTML content of a
-    particular URL and returns it
-    """
-    key = "result:{}".format(url)
-
-    response = requests.get(url)
-
-    _redis.set(key, response.text, ex=10)
-
-    return response.text
+    """ Obtain the HTML content of a  URL """
+    req = requests.get(url)
+    return req.text
